73bfeb64fe0923f0b6551685cac2378706c24c4c
naglaa77 <naglaafouz4@gmail.com>
2024-07-02 23:22:14 +0200

    Feat(Modify) : Modify select component

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -1,34 +1,62 @@
-import React from "react";
-import { TextField, Autocomplete } from "@mui/material";
-import {withJsonFormsControlProps} from "@jsonforms/react";
+import React, { useState } from "react";
+import { TextField, Autocomplete, Checkbox } from "@mui/material";
+import { withJsonFormsControlProps } from "@jsonforms/react";
 
+interface RatingControlProps {
+    data: any;
+    handleChange(path: string, value: any): void;
+    path: string;
+}
 
-const names = [
-    " Europe",
-    "Afrique",
+interface ContinentCountry {
+    continent: string;
+    countries: string[];
+}
+
+const continents: ContinentCountry[] = [
+    { continent: "Europe", countries: ["Belgium", "France"] },
+    { continent: "Afrique", countries: ["Egypt", "Syria"] }
 ];
 
+const Selector = ({ data, handleChange, path }: RatingControlProps) => {
+    const [selectedValues, setSelectedValues] = useState<string[]>(data || []);
 
-const Selector = () => {
+    const handleOnChange = (event: React.SyntheticEvent, value: string[]) => {
+        const lastSelectedValue = value[value.length - 1];
+        const continent = continents.find(c => c.continent === lastSelectedValue);
+        if (continent) {
+            // If a continent is selected, select all its countries
+            const newSelectedValues = Array.from(new Set([...value, ...continent.countries]));
+            setSelectedValues(newSelectedValues);
+            handleChange(path, newSelectedValues);
+        } else {
+            setSelectedValues(value);
+            handleChange(path, value);
+        }
+    };
+
+    const options = continents.flatMap(group => [group.continent, ...group.countries]);
+
     return (
-        <Autocomplete
-            sx={{ m: 1, width: 500 }}
-            multiple
-            options={names}
-            getOptionLabel={(option) => option}
-            disableCloseOnSelect
-            onChange={(event, value) => {
-                console.log("value", value);}
-            }
-            renderInput={(params) => (
-                <TextField
-                    {...params}
-                    variant="outlined"
-                    label="Multiple Autocomplete"
-                    placeholder="Multiple Autocomplete"
-                />
-            )}
-        />
+        <div>
+            <Autocomplete
+                sx={{ m: 1, width: 500 }}
+                multiple
+                options={options}
+                getOptionLabel={(option) => option}
+                renderOption={(props, option, { selected }) => (
+                    <li {...props}>
+                        <Checkbox style={{ marginRight: 8 }} checked={selected} />
+                        {option}
+                    </li>
+                )}
+                value={selectedValues}
+                onChange={handleOnChange}
+                renderInput={(params) => <TextField {...params} label="Select Continent and Countries" />}
+                disableCloseOnSelect
+            />
+        </div>
     );
-}
+};
+
 export default withJsonFormsControlProps(Selector);
